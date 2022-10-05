f98ad3d082fa6e0e75e1a0c53a898d0e19a2a253
kieutrongloc <kieutrongloc1298@gmail.com>
2022-10-06 06:17:37 +0700

    commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSubnav() {
+    var subnav = document.getElementById("subnav-cart-checkout");
+    if (subnav.style.display === "none") {
+        subnav.style.display = "block";
+    } else {
+        subnav.style.display = "none";
+    }
+  }
