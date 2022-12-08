e36c6a68d950b548e0311f22bb933ed54bd65070
kieutrongloc <kieutrongloc1298@gmail.com>
2022-12-08 16:56:03 +0700

    commit

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-function showSubnav() {
-    var subnav = document.getElementById("subnav-cart-checkout");
-    if (subnav.style.display === "none") {
-        subnav.style.display = "block";
-    } else {
-        subnav.style.display = "none";
+// function showSubnav() {
+//     var subnav = document.getElementById("subnav-cart-checkout");
+//     if (subnav.style.display === "none") {
+//         subnav.style.display = "block";
+//     } else {
+//         subnav.style.display = "none";
+//     }
+//   }
+
+//   function getTotal(arr) {
+//     let sum = 0;
+//     for(let i=0;i<arr.length;i++){
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+
+
+function run(object) {
+    var arr = [];
+    for (var key in object){
+        arr.push("Thuộc tính " + key + " có giá trị " + object[key]);
     }
-  }
+    return arr;
+}
+console.log(run({ name: 'Nguyen Van A', age: 16 }));
